233b30fa8d27bf8abe9de2c186e58e0f2ea2334c
heilorg <lhs01201@gmail.com>
2019-12-17 22:53:25 +0900

    구조 잡기, 개념 배우기, 흐름 파악하기

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Board extends React.Component {
+    renderSqure(i){
+        return <Square value={i} />;
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        const status = "Next Player : X";
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSqure(0)}
+                    {this.renderSqure(1)}
+                    {this.renderSqure(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSqure(3)}
+                    {this.renderSqure(4)}
+                    {this.renderSqure(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSqure(6)}
+                    {this.renderSqure(7)}
+                    {this.renderSqure(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square" onClick={() => alert("click")}>
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+class Game extends React.Component{
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <div>{/* todo */}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game/>,
+    document.getElementById('root')
+);
